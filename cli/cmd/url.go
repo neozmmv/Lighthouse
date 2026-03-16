925475473e4a8134150b0b1a17c9680b56460687
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-03-16 15:36:12 -0300

    CLI - onion url

--- cli/cmd/url.go
+++ cli/cmd/url.go
@@ -2,12 +2,27 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func getOnionUrl() (string, error) {
-	return "", nil
+	dir, err := getLighthouseDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to get lighthouse directory: %w", err)
+	}
+
+	c := exec.Command("docker", "exec", "lighthouse-tor", "cat", "/var/lib/tor/hidden_service/hostname")
+	c.Dir = dir
+	output, err := c.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to get .onion URL: %w", err)
+	}
+
+	url := strings.TrimSpace(string(output))
+	return url, nil
 }
 
 
@@ -19,9 +34,15 @@ var urlCmd = &cobra.Command{
 		if !running {
 			return fmt.Errorf("Lighthouse is not running. Please start it with 'lighthouse up' first.")
 		}
+		url, err := getOnionUrl()
+		if err != nil {
+			return fmt.Errorf("failed to get .onion URL: %w", err)
+		}
+		fmt.Println(url)
 		return nil
 	},
 }
+
 func init() {
 	rootCmd.AddCommand(urlCmd)
 }
