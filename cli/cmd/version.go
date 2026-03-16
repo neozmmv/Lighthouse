d8499338af3d21a3f0a898665df8e9613ff0b47e
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-03-16 16:42:10 -0300

    fix version command formatting

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use: "version",
 	Short: "Print the version of Lighthouse",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Lighthouse CLI v%s\n", version)
+		fmt.Printf("Lighthouse CLI %s\n", version)
 	},
 }
 
