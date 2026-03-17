549306431ab169e0c0e64c514d60b32f06f8ebef
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-03-17 17:24:44 -0300

    CLI - download with --here and --remove flags

--- cli/cmd/download.go
+++ cli/cmd/download.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -13,6 +14,7 @@ import (
 )
 
 var here bool
+var remove bool
 
 func downloadFile(f FileInfo, destDir string) error {
     apiURL := fmt.Sprintf("http://localhost:8000/api/files/%s/download", f.FileId)
@@ -63,6 +65,27 @@ func downloadFile(f FileInfo, destDir string) error {
     return nil
 }
 
+func deleteFile(fileId string) error {
+    apiURL := fmt.Sprintf("http://localhost:8000/api/files/%s", url.PathEscape(fileId))
+
+    req, err := http.NewRequest(http.MethodDelete, apiURL, nil)
+    if err != nil {
+        return err
+    }
+
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return fmt.Errorf("Error deleting file: status %d", resp.StatusCode)
+    }
+
+    return nil
+}
+
 var downloadCmd = &cobra.Command{
     Use:   "download [index]",
     Short: "Downloads a file from the bucket using the index returned by 'lighthouse files'",
@@ -96,11 +119,21 @@ var downloadCmd = &cobra.Command{
             destDir = filepath.Join(home, "Downloads")
         }
 
-        return downloadFile(selected, destDir)
+        if err := downloadFile(selected, destDir); err != nil {
+			return err
+		}
+
+		if remove {
+			if err := deleteFile(selected.FileId); err != nil {
+                return fmt.Errorf("Error removing file from the bucket: %w", err)
+            }
+		}
+		return nil
     },
 }
 
 func init() {
     rootCmd.AddCommand(downloadCmd)
-    downloadCmd.Flags().BoolVar(&here, "here", false, "save in current directory")
+    downloadCmd.Flags().BoolVar(&here, "here", false, "save in current directory (defaults to ~/Downloads)")
+	downloadCmd.Flags().BoolVar(&remove, "remove", false, "removes the file from the bucket after downloading")
 }
